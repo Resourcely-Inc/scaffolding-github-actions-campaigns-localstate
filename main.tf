0ec64bb92e96004aeb4684e8ade54f8e409009b8
Emily Bragg <ebragg09@gmail.com>
2024-10-11 14:37:01 -0400

    base contents

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "null_resource" "foo" {
     foo = var.project
   }
 }
+
+resource "aws_s3_bucket" "bucket-1" {
+	bucket = "foo-bucket"
+        tags = {
+    		Name        = "My bucket"
+    		Environment = "Dev"
+  	}
+}
