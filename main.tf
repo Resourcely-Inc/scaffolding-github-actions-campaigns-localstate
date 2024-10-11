f755e4baf23fc73c3b076d4b7b18912bf8748385
Emily Bragg <ebragg09@gmail.com>
2024-10-11 14:40:54 -0400

    [Resourcely Instance] add bucket 1

--- main.tf
+++ main.tf
@@ -5,3 +5,34 @@ resource "null_resource" "foo" {
     bar = var.project
   }
 }
+
+resource "aws_s3_bucket" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
+  bucket = "resourcely-foobucket"
+  tags = {
+    owner = "infra"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
+  bucket                  = aws_s3_bucket.resourcely-foobucket_ccYTHXSP6VpVTaLd.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
+  bucket = aws_s3_bucket.resourcely-foobucket_ccYTHXSP6VpVTaLd.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
+  bucket = aws_s3_bucket.resourcely-foobucket_ccYTHXSP6VpVTaLd.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
