240fe937d1635c2eed4a9b983ed4c7fc1735b573
Emily Bragg <ebragg09@gmail.com>
2024-10-28 19:46:27 -0400

    Updated via Resourcely

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_s3_bucket" "foo-10-28" {
   acl    = "authenticated-read"
 }
 
-resource "aws_s3_bucket" "foo-10-28-2" {
-  bucket = "sample2"
+resource "aws_s3_bucket" "foo" {
+  bucket = "sample3"
   acl    = "authenticated-read"
 }
