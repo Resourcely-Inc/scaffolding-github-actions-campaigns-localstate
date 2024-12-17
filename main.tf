14c6fd6c53a31be47941e0192b5f236d897e55b6
Aladdin Almubayed <522779+alaamub@users.noreply.github.com>
2024-12-17 14:10:51 -0800

    [Resourcely Instance] test

--- main.tf
+++ main.tf
@@ -102,3 +102,35 @@ resource "aws_s3_bucket" "foo-10-29" {
     teaf = "footeam"
   }
 }
+
+resource "aws_s3_bucket" "test_AWy9kBEPqvC8u326" {
+  bucket = "test"
+  acl    = "authenticated-read"
+  tags = {
+    owner = "security"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "test_AWy9kBEPqvC8u326" {
+  bucket                  = aws_s3_bucket.test_AWy9kBEPqvC8u326.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "test_AWy9kBEPqvC8u326" {
+  bucket = aws_s3_bucket.test_AWy9kBEPqvC8u326.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "test_AWy9kBEPqvC8u326" {
+  bucket = aws_s3_bucket.test_AWy9kBEPqvC8u326.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
--- resourcely.tf
+++ resourcely.tf
@@ -1,11 +1,19 @@
+// TODO: a real explainer comment
 resource "terraform_data" "resourcely_context_answers" {
   input = {
     "version": 1,
 
     "data": [
       {
-        "$applies_to": "resource.aws_s3_bucket_public_access_block.resourcely-foobucket_ccYTHXSP6VpVTaLd",
-        "applies_to_all_multi": ["x", "y"],
+        "$applies_to": [
+          "resource.aws_s3_bucket.test_AWy9kBEPqvC8u326",
+          "resource.aws_s3_bucket_public_access_block.test_AWy9kBEPqvC8u326",
+          "resource.aws_s3_bucket_ownership_controls.test_AWy9kBEPqvC8u326",
+          "resource.aws_s3_bucket_versioning.test_AWy9kBEPqvC8u326"
+        ],
+        "applies_to_all_multi": "x",
+        "owner": "security",
+        "purpose": "backup"
       }
     ]
   }
