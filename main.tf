d79fa798fabf3607287895b964b31ec7ee7efac7
dd12440 <131015934+dd12440@users.noreply.github.com>
2024-12-16 19:10:48 -0600

    Creating a pull request to evaluate remediations

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "null_resource" "foo" {
 }
 
 resource "aws_s3_bucket" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
-  bucket = "resourcely-foobucket"
+  bucket = "sample2"
   tags = {
     owner = "infra"
   }
