33cc20f52d720fe134b70ed66deb31faa550230f
dd12440 <131015934+dd12440@users.noreply.github.com>
2024-12-17 16:05:47 -0600

    Create resourcely.tf

---
+++ resourcely.tf
@@ -0,0 +1,12 @@
+resource "terraform_data" "resourcely_context_answers" {
+  input = {
+    "version": 1,
+
+    "data": [
+      {
+        "$applies_to": "resource.aws_s3_bucket_public_access_block.resourcely-foobucket_ccYTHXSP6VpVTaLd",
+        "applies_to_all_multi": ["x", "y"],
+      }
+    ]
+  }
+}
