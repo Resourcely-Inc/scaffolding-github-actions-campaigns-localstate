e50cbe204facafc19cd6c1c5ace79991dbc9ea6c
dd12440 <131015934+dd12440@users.noreply.github.com>
2024-12-20 09:36:03 -0600

    Draft: evaluation run for remediation

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "null_resource" "foo" {
 }
 
 resource "aws_s3_bucket" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
-  bucket = "resourcely-foobucket222"
+  bucket = "resourcely-foobucket2222"
   tags = {
     owner = "infra"
   }
