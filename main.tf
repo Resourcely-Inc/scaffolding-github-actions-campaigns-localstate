8e338d28fce2745e2027074c6e929cb7a46f0d77
dd12440 <131015934+dd12440@users.noreply.github.com>
2025-01-10 15:32:19 -0600

    Draft: evaluation run for remediation

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_versioning" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
 }
 
 resource "aws_s3_bucket" "bucket-1" {
- bucket = "resourcely-foo-bucket-2"
+ bucket = "not-resourcely-foo-bucket-2"
    tags = {
       Name        = "My bucket"
       Environment = "Dev"
