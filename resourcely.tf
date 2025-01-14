a0fde76487a44a7ebc4b2bd012bd49757be4c585
dd12440 <131015934+dd12440@users.noreply.github.com>
2025-01-14 09:45:11 -0600

    Creating a pull request to evaluate remediations

--- resourcely.tf
+++ resourcely.tf
@@ -1,3 +1,4 @@
+// TODO: a real explainer comment
 resource "terraform_data" "resourcely_context_answers" {
   input = {
     "version": 1,
@@ -5,12 +6,17 @@ resource "terraform_data" "resourcely_context_answers" {
     "data": [
       {
         "$applies_to": "resource.aws_s3_bucket_public_access_block.resourcely-foobucket_ccYTHXSP6VpVTaLd",
-        "applies_to_all_multi": ["x", "y"],
+        "applies_to_all_multi": ["x", "y"]
       },
       {
         "$applies_to": "resource.aws_s3_bucket.resourcely-foobucket_ccYTHXSP6VpVTaLd",
+        "purpose": "backup",
         "applies_to_all_multi": ["x", "y"],
-        "purpose": "backup"
+        "global_multi_select": "multi-a",
+        "tier": "200",
+        "global_single_select": "global-a",
+        "owner": "network",
+        "global_text": "text"
       }
     ]
   }
