3e0b660c6f020626bc6beeff981cb0cc121e4f56
dd12440 <131015934+dd12440@users.noreply.github.com>
2025-01-15 14:25:40 -0600

    Creating a pull request to evaluate remediations

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
 
 resource "aws_s3_bucket_public_access_block" "resourcely-foobucket_ccYTHXSP6VpVTaLd" {
   bucket                  = aws_s3_bucket.resourcely-foobucket_ccYTHXSP6VpVTaLd.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
