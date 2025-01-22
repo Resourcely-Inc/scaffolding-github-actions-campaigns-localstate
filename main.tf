d555945e8502ddf4d1488990b159bdb1bf2885b4
dd12440 <131015934+dd12440@users.noreply.github.com>
2025-01-22 16:58:10 -0600

    Draft: evaluation run for remediation

--- main.tf
+++ main.tf
@@ -77,15 +77,13 @@ resource "aws_s3_bucket_versioning" "resourcely-bucket_VBTsxzFV4pziKNEi" {
   }
 }
 
-...
-
 resource "aws_s3_bucket" "foo-10-28" {
   bucket = "sample2"
   acl    = "authenticated-read"
 }
 
 resource "aws_s3_bucket" "foo-10-28-2" {
-  bucket = "sample3"
+  bucket = "resourcely-sample3"
   acl    = "authenticated-read"
 }
 
